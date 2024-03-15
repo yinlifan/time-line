69447736d5d2ce0eeeabd1d9adfb6bc0ed8cbdfb
yinlifan <yinlifan@jiefanglian.com>
2024-03-15 10:47:47 +0800

    up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,29 @@
+<template>
+  <main>
+    主页面组件
+    <div class="boarding">
+      <TimeLine v-model:timeArray="vm.currentTime" />
+    </div>
+    ---------------- 父组件中的双向绑定
+    {{ vm.currentTime }}
+    <br />
+    ---------------- 父组件中改变小时值
+    <el-input
+      v-model="vm.currentTime[0]"
+      placeholder="111"
+      size="large"
+      clearable
+    ></el-input>
+    ---------------- 父组件中改变分钟值
+    <el-input
+      v-model="vm.currentTime[1]"
+      placeholder="111"
+      size="large"
+      clearable
+    ></el-input>
+  </main>
+</template>
+
 <script setup>
 import { defineAsyncComponent, reactive, ref, watch } from "vue";
 const TimeLine = defineAsyncComponent(() =>
@@ -25,31 +51,6 @@ watch(
 );
 </script>
 
-<template>
-  <main>
-    主页面组件
-    <div class="boarding">
-      <TimeLine v-model:timeArray="vm.currentTime" />
-    </div>
-    ---------------- 父组件中的双向绑定
-    {{ vm.currentTime }}
-    <br />
-    ---------------- 父组件中改变小时值
-    <el-input
-      v-model="vm.currentTime[0]"
-      placeholder="111"
-      size="large"
-      clearable
-    ></el-input>
-    ---------------- 父组件中改变分钟值
-    <el-input
-      v-model="vm.currentTime[1]"
-      placeholder="111"
-      size="large"
-      clearable
-    ></el-input>
-  </main>
-</template>
 <style lang="scss" scoped>
 .boarding {
   width: 700px;
